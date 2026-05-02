9f96218de879c67aab787a34eaebf4fddb81a688
Antony Mwangi <antonymwangiw85@gmail.com>
2026-05-02 05:44:05 -0500

    analytics page

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -256,7 +256,7 @@ export default function AnalyticsPage() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
 
         <Stat title="Revenue" value={`Ksh ${data.totalRevenue}`} />
-        <Stat title="Profit" value={`Ksh ${data.totalProfit}`} />
+        {/* <Stat title="Profit" value={`Ksh ${data.totalProfit}`} /> */}
         <Stat title="Sales" value={data.totalSales} />
         <Stat
           title="Trend"
