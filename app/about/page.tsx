d9dc5c2c9b196fe3fca6fec5e7641b95edb773f5
Antony Mwangi <antonymwangiw85@gmail.com>
2026-05-13 13:53:55 -0500

    login redirect

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -37,7 +37,7 @@ export default function AboutPage() {
         </Link>
 
         <Link
-          href="/dashboard"
+          href="/login"
           className="text-sm font-semibold bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
         >
           Go to Dashboard
