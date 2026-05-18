6b36ae4bd5bbe91c2e3bc2e876fde20ddb4f3d5d
Antony Mwangi <antonymwangiw85@gmail.com>
2026-05-18 08:51:28 -0500

    admin

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import StatsCard from "@/app/admin/components/StatsCard";
+ import StatsCard from "@/app/admin/components/StatsCard";
 
 type Analytics = {
   totalUsers: number;
